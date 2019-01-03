9683792f98d042519260267cc2a559fa81ca4acd
iamthejedi <folabodunwa@gmail.com>
2019-01-03 20:43:17 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+"""
+This following lines of code is the metadata of Championships, an album released by American 
+rapper Meek Mill. It also includes other interesting details which are not typically
+metadata information but which will I though were interesting enough to be included.
+
+It is my homework following the lecture on "Variables"; the first lecture on my quest to
+becoming an python developer! 
+"""
+Artiste = "Meek Mill"
+Album = "Championships"
+ReleaseDate = "30 Novemeber 2018"
+NumberOfTracksOnAlbum = 19
+DurationOfAlbumInMinutes = 69 
+Format = "MP3"
+Genre = "Hip-Hop & Rap"
+Label = "MMG/Atlantic"
+ParentalAdvisory = "Explicit Lyrics"
+PriceOfAlbumInDollars = 9.99
+NumberOfTracksFromAlbumOnBillboardTop100 = 2
+PositionOfHighestChartingTrackonBillboardTop100 = 36
+NameOfHighestChartingTrackonBillboardTop100 = "Going Bad"
+NumberOfArtistesFearuredOnAlbum = 15
+AlbumRatingAsReviewedByPitchfork = "7.7/10"
+print(Artiste)
+print(Album)
+print(ReleaseDate)
+print(NumberOfTracksOnAlbum)
+print(DurationOfAlbumInMinutes)
+print(Format)
+print(Genre)
+print(Label)
+print(ParentalAdvisory)
+print(PriceOfAlbumInDollars)
+print(NumberOfTracksFromAlbumOnBillboardTop100)
+print(PositionOfHighestChartingTrackonBillboardTop100)
+print(NameOfHighestChartingTrackonBillboardTop100)
+print(NumberOfArtistesFearuredOnAlbum)
+print(AlbumRatingAsReviewedByPitchfork)
+
+"""
+Artiste - Meek Mill, real name, Robert Rihmeek Williams, is an American rapper
+and activist born and raised in Philadelphia
+
+Album - Championships is Meek Mill's fourth studio album. The album received 
+positive reviews from critics, selling 229,000 album-equivalent units in its first week
+
+Label - Meek Mill is signed to Maybach Music Group (MMG) which is a record label founded by
+another American rapper, Rick Ross. Albums released under MMG are distributed by Atlantic Records
+
+Price Of Album In Dollars - The aolbum retails for $9.99 on Apple Music
+
+Number Of Tracks From Album On Billboard Top 100 - The album has 2 tracks (Going Bad and Dangerous) currently 
+charting on the Billboard Top 100 at 36 and 62 respectively
+
+Number Of Artistes Fearured On Album - Meek Mill featured a number of other artistes on the albu  including 
+Cardi B, Drake, Rick Ross and Jay-Z
+
+"""
+
